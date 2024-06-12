4db1e90a3dfe58feab1f3d40295652b58b1217c1
BeatrizSelymes <164294239+BeatrizSelymes@users.noreply.github.com>
2024-06-12 23:48:56 +0000

    12/06/24

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-const perguntas = [
+const perguntas =[
   {
-    enunciado: "Qual é a cor do esmalte da Gaby?",
-    alternativas: [
-      "vermelho obssessão",
-      "marrom café"
+    enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele tambem gera imagens e audios hiper-realistas. Qual o seu primeiro pensamento?",
+ alternativas: [
+      "Isso é assustador!",
+      "Isso é maravilhoso!"
     ]
   },
 
   {
-    enunciado: "Quem é o melhor vilão Jason ou Fred Krügger",
+    enunciado: "Com a descoberta dessa tecnologia, chamada inteligencia artificial (IA), uma professora de tecnologia de uma escola decidiu fazer uma sequência de aulas sobre essa ferramenta. No fimde uma aula ela oede que você escreva um trabalho sobre o uso da IA em sala de aula. Qual atitude você toma?",
     alternativas: [
-      "Resposta 1",
-      "Resposta 2"
+      "Utiliza uma ferramenta da busca na internet que utiliza IA para que ela ajude a encontrar informções",
+      "Jason",
     ]
   },
 
@@ -49,15 +49,20 @@ const perguntas = [
 let atual = 0;
 let perguntaAtual;
 
-function mostraAlternativas () {
-  for (const alternativa of perguntaAtual.alternativas){
-    
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+    caixaAlternativas.appendChild(botaoAlternativas);
   }
 }
 
-function mostraPergunta(){
+
+function mostraPergunta() {
   perguntaAtual = perguntas[atual];
   caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
+
 }
 
 mostraPergunta();
